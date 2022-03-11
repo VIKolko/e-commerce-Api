403e3409369b203837becec69b659e860b0adb0f
Kolko Valentin <valiant9191@gmail.com>
2022-03-11 11:36:56 +0300

    authorization + verification

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-const userRoute = require("./rootes/user");
-const authRoute = require("./rootes/auth");
+const jsonParser = express.json();
+// routes 
+const userRoute = require('./rootes/user');
+const authRoute = require('./rootes/auth');
+const productRoute = require('./rootes/product');
+
+dotenv.config()
+const port = process.env.PORT||5000;
+const mongoUri = process.env.MONGO_URL
+
+mongoose.connect(mongoUri)
+.then(()=>console.log('database connected'))
+.catch((err)=>{console.log(err)})
 
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connected!"))
-  .catch((err) => {
-    console.log(err);
-  });
+app.use(express.json())
+app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+app.use("/api/products",productRoute)
 
-app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-// app.use("/api/products", productRoute);
-// app.use("/api/carts", cartRoute);
-// app.use("/api/orders", orderRoute);
-// app.use("/api/checkout", stripeRoute);
+// check 
+app.get('/api/test',()=>console.log('tested ok'))
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server ok!");
-});
+app.listen(port,()=>{
+  console.log(`server starts on port ${port}`)
+})
--- rootes/verifyToken.js
+++ rootes/verifyToken.js
@@ -1,30 +1,41 @@
-const jwt = require('jsonwebtoken');
-
-
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.token
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-            if (err) res.status(403).json('token not valid!')
-            req.user = user;
-            next()
-        })
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("token invalid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("not authenticated!");
+  }
+};
+
+const verifyTokenAuthorisation = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
     } else {
-        return res.status(401).json('not authenticated!')
+      res.status(403).json("Denied, not alowed");
     }
+  });
+};
 
-}
+const verifyTokenAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Denied, not alowed");
+    }
+  });
+};
 
-const verifyTokenAuthorisation = (req,res,next)=>{
-    verifyToken(req,res,()=>{
-        if (req.user.id === req.params.id || req.user.isAdmin) {
-            next()
-        } else {
-            res.status(403).json('Denied, not alowed')
-        }
-    })
-}
-
-module.exports = { verifyToken,verifyTokenAuthorisation }
+module.exports = {
+  verifyToken,
+  verifyTokenAuthorisation,
+  verifyTokenAdmin,
+};
