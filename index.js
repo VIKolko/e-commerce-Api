be2cb8adffb7e72009063eda4cdadada1b0702de
Kolko Valentin <valiant9191@gmail.com>
2022-03-12 18:33:32 +0300

    add values to schema product

--- index.js
+++ index.js
@@ -3,34 +3,38 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const jsonParser = express.json();
-// routes 
-const userRoute = require('./rootes/user');
-const authRoute = require('./rootes/auth');
-const productRoute = require('./rootes/product');
-const cartRoute = require('./rootes/cart');
-const orderRoute = require('./rootes/order');
-const stripeRoute = require('./rootes/stripe');
+// routes
+const userRoute = require("./rootes/user");
+const authRoute = require("./rootes/auth");
+const productRoute = require("./rootes/product");
+const cartRoute = require("./rootes/cart");
+const orderRoute = require("./rootes/order");
+const stripeRoute = require("./rootes/stripe");
+const cors = require("cors");
 
-dotenv.config()
-const port = process.env.PORT||5000;
-const mongoUri = process.env.MONGO_URL
+dotenv.config();
+const port = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URL;
 
-mongoose.connect(mongoUri)
-.then(()=>console.log('database connected'))
-.catch((err)=>{console.log(err)})
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log("database connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/stripe", stripeRoute);
 
-app.use(express.json())
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/products",productRoute)
-app.use("/api/carts",cartRoute)
-app.use("/api/orders",orderRoute)
-app.use("/api/stripe",stripeRoute)
+// check
+// app.get("/api/test", () => console.log("tested ok"));
 
-// check 
-app.get('/api/test',()=>console.log('tested ok'))
-
-app.listen(port,()=>{
-  console.log(`server starts on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server starts on port ${port}`);
+});
