fb02d5e370c2b939947913a051802122f4b4c9a0
Kolko Valentin <valiant9191@gmail.com>
2022-03-22 11:00:43 +0300

    fix

--- rootes/order.js
+++ rootes/order.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Order = require("../models/order");
+const Order = require("../models/Order");
 const CryptoJS = require("crypto-js");
 const dotenv = require("dotenv");
 
