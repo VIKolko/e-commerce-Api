b0264c489589b4081210620f96e1ad910ef01b18
Kolko Valentin <valiant9191@gmail.com>
2022-03-23 17:31:30 +0300

    add fullname in authorization

--- rootes/auth.js
+++ rootes/auth.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 //REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
+    fullname: req.body.fullname,
     username: req.body.username,
     email: req.body.email,
     password: CryptoJS.AES.encrypt(
