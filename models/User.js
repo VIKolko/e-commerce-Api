a30f6ba5f1e2ad05ed9879f30557b294235c3683
Kolko Valentin <valiant9191@gmail.com>
2022-03-23 18:50:55 +0300

    model user add fields

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
 
-    fullName:{type: Object},
+    fullname:{type: Object},
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
